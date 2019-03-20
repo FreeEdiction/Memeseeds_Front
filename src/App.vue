4a8c9c33566f6ea94805fb9516a8a5b84b9438f7
FreeEdiction <romasokur99@gmail.com>
2019-03-20 22:32:40 +0200

    Project

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+
+  <router-view></router-view>
+</template>
+
+<script>
+  import VueRouter from './router.js'
+
+  //Vue.use(VueRouter);
+  import signUp from './components/SignUp.vue';
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  },
+  components:
+    {
+      signUp:signUp
+    }
+}
+</script>
+
+<style>
+  body {
+    background: url("/src/css/images/background.png") center no-repeat;
+    font-family: 'Aqua Grotesque', regular;
+  }
+
+  .logo img {
+    width: 100px;
+    height: 100px;
+  }
+</style>
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from 'vue';
+import App from './App.vue';
+import BootstrapVue from "bootstrap-vue"
+import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap-vue/dist/bootstrap-vue.css"
+import router from './router.js'
+
+Vue.use(BootstrapVue)
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+})
